5718dabe9272f741ab758ab923a372cf19efe603
Mr.King <1120427301@qq.com>
2019-12-02 19:41:29 +0800

    recommend

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+/* eslint indent: "error" */
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+})
